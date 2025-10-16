61abfcebe1832e26a6895bf586ee03ecddce0b58
jetender <37366047+jetender@users.noreply.github.com>
2025-10-16 16:04:15 +0530

    Delete cleanup_images.sh

--- cleanup_images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE_DIR="/media/vdata"
-
-echo "Starting cleanup in: $BASE_DIR"
-
-# Delete .jpg and .jpeg files older than 30 days
-echo "Deleting .jpg and .jpeg files older than 30 days..."
-find "$BASE_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -mtime +30 -exec rm -v {} \;
