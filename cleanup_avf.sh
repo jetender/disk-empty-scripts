e24ed7496ed8d9e2a81736931acf9e445f41a32f
jetender <37366047+jetender@users.noreply.github.com>
2025-10-23 12:11:16 +0530

    Update cleanup_avf.sh

--- cleanup_avf.sh
+++ cleanup_avf.sh
@@ -5,6 +5,6 @@ BASE_DIR="/media/vdata"
 
 # Delete .avf and .evaf files older than 1 day
 echo "Deleting .avf and .evaf files older than 1 day..."
-find "$BASE_DIR" -type f \( -iname "*.avf" -o -iname "*.evaf" \) -mtime +1 -exec rm -v {} \;
+find "$BASE_DIR" -type f \( -iname "*.avf" -o -iname "*.evaf" \) -mtime +0 -exec rm -v {} \;
 
 echo "Cleanup complete."
