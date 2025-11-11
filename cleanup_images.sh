ec570735bde3c84aaa978be99b32d44cac81514a
jetender <37366047+jetender@users.noreply.github.com>
2025-11-11 15:34:12 +0530

    Update cleanup_images.sh

--- cleanup_images.sh
+++ cleanup_images.sh
@@ -6,4 +6,4 @@ echo "Starting cleanup in: $BASE_DIR"
 
 # Delete .jpg and .jpeg files older than 30 days
 echo "Deleting .jpg and .jpeg files older than 30 days..."
-find "$BASE_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -mtime +28 -exec rm -v {} \;
+find "$BASE_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -mtime +27 -exec rm -v {} \;
